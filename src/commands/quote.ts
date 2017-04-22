f8796c8ecb44c1750dcbfda17af0384dbab4499e
nsteinhafel <nsteinhafel@gmail.com>
2017-04-21 23:03:19 -0700

    continued quotes... need to transition to mongo

--- src/commands/quote.ts
+++ src/commands/quote.ts
@@ -1,11 +1,33 @@
 
+import * as fs from 'fs';
+
 import { Util } from '../util'
 import { Command } from './command'
 
+export interface QuoteRecord {
+    /** The author of quote. */
+    author: string;
+
+    /** The quote itself. */
+    quote: string;
+}
+
 export class Quote extends Command {
 
-    /** Reply with "Hello!". */
+    /** Reply with a quote. */
     async run(): Promise<void> {
-        // TODO
+        // TODO move to db and remove fs dependency.
+
+        fs.readFile(__dirname + '/../seed/quotes.json', (err, data) => {
+            if (err) throw err;
+
+            // Parse quotes from file.
+            const quotes = <QuoteRecord[]>JSON.parse(data.toString());
+
+            // Math.random cannot return 1...
+            const quote = quotes[Math.floor(Math.random() * quotes.length)]
+
+            this.message.channel.sendMessage(`"${quote.quote}" - ${quote.author}`);
+        })
     }
 }
